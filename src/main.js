d7feb365dabb8a8529693cb841f029849fec6432
LuIntelli <eifezulumba@gmail.com>
2025-05-07 12:08:18 +0100

    initialized the project structure for the online examination monitoring system

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import "./style.css";
+import "primeicons/primeicons.css";
+import App from "./App.vue";
+
+
+const application = createApp(App);
+application.mount("#app")
